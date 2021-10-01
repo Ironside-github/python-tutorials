b95b460763cc5ca7838bc8074d0ae4761f2bf51c
Amit Thakur <amit4466thakur@gmail.com>
2021-10-01 13:57:46 +0530

    second commit

---
+++ chapter 4 lists and tupples/practice setlist1 q2.py
@@ -0,0 +1,11 @@
+#to accept marks from 6 students and display them in sorted manner
+s1= int(input("enter your marks\n"))
+s2 = int(input("enter your marks\n"))
+s3 = int(input("enter your marks\n"))
+s4 = int(input("enter your marks\n"))
+s5 = int(input("enter your marks\n"))
+s6 = int(input("enter your marks\n"))
+marks = [s1,s2,s3,s4,s5,s6]
+print(marks)
+marks.sort()
+print (marks)
